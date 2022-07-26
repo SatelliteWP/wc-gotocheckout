48f5f0edc8fa45a90f6abeb5414d8002f84bb60a
Maxime Jobin <maximejobin@users.noreply.github.com>
2022-07-26 17:31:24 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/** 
+ * Plugin Name: Go directly to checkout after a 'Add to cart'
+ * Description: Skip the cart and go to checkout
+ * Version: 1.0
+ * Author: SatelliteWP
+ * License: GPL v3
+ */
+
+if ( ! defined( 'ABSPATH' ) ) die( 'No' );
+
+/**
+ * Skip the cart and redirect to checkout
+ *
+ * @param string $url     URL to redirect to after a 'Add to cart'
+ * @param mixed  $product WC_Product
+ *
+ * @return string URL
+ */
+function swp_woocommerce_add_to_cart_redirect( $url, $product ) {
+	
+	// Redirect to checkout if ?gotocheckout=true is found in the URL
+	if ( isset( $_GET['gotocheckout'] ) && $_GET['gotocheckout'] == 'true' ) {
+		return wc_get_checkout_url();
+	}
+	
+	return $url;
+}
+add_filter( 'woocommerce_add_to_cart_redirect', 'swp_woocommerce_add_to_cart_redirect', 10, 2 );
